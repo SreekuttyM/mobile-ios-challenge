b70894d5bbc6d063d63cea317553378ebcbdb9a5
Sreekutty <sreekuttystar@gmail.com>
2022-05-01 13:45:10 +0400

    project setup added

---
+++ ARAMO-APP/ARAMO-APP/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ARAMO-APP
+//
+//  Created by Sreekutty Maya on 01/05/2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
